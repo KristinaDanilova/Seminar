331ce229b4758d1cc009218e8d2ffbb7824fb27a
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-23 20:36:05 +0300

    Day

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите день недели: ");
+int day = int.Parse(Console.ReadLine());
+if (0 < day && day < 8)
+{
+   if (day == 1) Console.WriteLine("Понедельник");
+   if (day == 2) Console.WriteLine("Вторник");
+   if (day == 3) Console.WriteLine("Среда");
+   if (day == 4) Console.WriteLine("Четверг");
+   if (day == 5) Console.WriteLine("Пятница");
+   if (day == 6) Console.WriteLine("Суббота");
+   if (day == 7) Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Не правильный день недели");
+}
