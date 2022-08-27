eba6a9f617b0ef6149fa1337c1a8d31d6d8e76fa
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-27 16:29:38 +0300

    Добавили решения по заданиям 9, 11, 12, 14, 16

---
+++ Task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+if(num1 % 7 == 0 && num1 % 23 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно");
+}
