25e8ba3f806d60413ed10031abea23cde9baf317
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-30 11:31:25 +0300

    Task 17,18,21,22

---
+++ Seminar3/Task17/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Write("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+else
+if (x < 0 && y > 0)
+{
+    Console.Write("2");
+}
+if (x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+else
+if (x < 0 && y > 0)
+{
+    Console.Write("2");
+}
+if (x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+else
+if (x < 0 && y > 0)
+{
+    Console.Write("2");
+}
+if (x > 0 && y > 0)
+{
+    Console.Write("1");
+}
+else
+if (x < 0 && y > 0)
+{
+    Console.Write("2");
+}
+else
+if (x < 0 && y < 0)
+{
+    Console.Write("3");
+}
+else
+if (x > 0 && y < 0)
+{
+    Console.Write("4");
+}
---
+++ Seminar3/Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Таблица квадратов чисел от 1 до N
+Console.WriteLine("Введите число: ");
+int numberN = int.Parse(Console.ReadLine());
+int count = 1;
+while (count <= numberN)
+{
+    Console.Write($"{count*count}, ");
+    count++;
+}
+Console.WriteLine();
+for (int i = 1; i <= numberN; i++)
+{
+    Console.Write($"{i*i}, ");
+}
+Console.WriteLine();
