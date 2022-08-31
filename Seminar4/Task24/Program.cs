72e4fe4657cba7f5d9eceaf1f53acce43df28fe2
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-31 14:32:16 +0300

    сумма чисел от 1 до а

---
+++ Seminar4/Task24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число А
+// и выдает сумму чисел от 1 до А. (7=>28; 4=>10; 8=>36)
+
+int GetNums (int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число А: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{GetNums(num)}");
+
+
