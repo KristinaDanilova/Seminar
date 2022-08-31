083b1c6c66fa87b313b3aa47238fa35f5f00dd22
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-31 16:03:21 +0300

    произведение чисел от 1 до n

---
+++ Seminar4/Task28/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N
+// и выдает произведение чисел от 1 до N. 4=>24. 5=>120.
+
+int xNumber(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{xNumber(number)}");
+
