bfe491c3fba2e7a7d1ad26d91b17e3e878d9a5ea
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-31 16:35:31 +0300

    массив из 8 эл. в виде 0 и 1

---
+++ Seminar4/Task30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+
+int [] InitArray()
+{
+    int [] arr = new int[8];
+    for (int i=0; i<arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void ReadArray(int [] arr)
+{
+ for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+ReadArray(InitArray());
+
+Console.WriteLine(String.Join(", ", InitArray()));
