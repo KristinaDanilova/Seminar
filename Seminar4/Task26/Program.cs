ccc77aa0c05585141cb2598813737526e0a02bba
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-08-31 15:09:18 +0300

    количество цифр в числе

---
+++ Seminar4/Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдает количество цифр в числе. 456 => 3. 78 => 2.
+
+int colNumber(int num)
+{
+    int col = 0;
+    while (num!= 0)
+    {
+        col++;
+        num = num / 10;
+    }
+    return col;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{colNumber(number)}");
+
