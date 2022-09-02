bf7ffedb6614ba059fad96d529b8b6c68dc105f1
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-02 15:41:13 +0300

    сумма полож и отриц чисел в массиве

---
+++ Seminar5/Task31/Program.cs
@@ -0,0 +1,54 @@
+﻿int [] GetArray(int size, int minValue, int maxValue)
+{
+    int [] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] PositiveAndNegativeSum(int [] array)
+{
+    int [] res = new int [2];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            res[0] += array [i];
+        }
+        else
+        {
+            res[1] += array [i];
+        }
+    }
+    return res;
+}
+
+void PostiveSum(int[] array)
+{
+    int positiveSum = 0;
+
+    foreach(int el in array)
+    {
+        positiveSum += el > 0 ? el : 0;
+        if (el > 0)
+        {
+            positiveSum += el;
+        }
+        else
+        {
+            positiveSum += 0;
+        }
+    }
+    Console.WriteLine($"{positiveSum}");
+}
+
+
+int[] myArray = GetArray(10, -100, 100);
+int[] result = PositiveAndNegativeSum(myArray);
+Console.WriteLine(String.Join(" ", myArray));
+Console.WriteLine(String.Join(" ", result));
+PostiveSum(myArray);
