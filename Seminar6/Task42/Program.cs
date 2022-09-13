c14c047729ecb4daa38bb1c67db3ce6c165ae0e5
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-13 12:28:29 +0300

    Преобразовать десятичное число в двоичное

--- Seminar6/Task42/Program.cs
+++ Seminar6/Task42/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Преобразовывать десятичное число в двоичное
+
+string Binary(int num)
+{
+    string binaryNum = String.Empty;
+    while (num > 0)
+    {
+        if (num % 2 == 1) binaryNum += "1";
+        else binaryNum += "0";
+        num /= 2;
+    }
+    return binaryNum;
+}
+
+Console.WriteLine("Введите число: ");
+int decNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Binary(decNum));
