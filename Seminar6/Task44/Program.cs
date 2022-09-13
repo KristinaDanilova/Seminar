86df45742e956346b8e48a1db3890f30702d037d
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-13 19:30:30 +0300

    Первые n чисел Фибоначчи

---
+++ Seminar6/Task44/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выведите первые N чисел Фибоначчи (0 и 1)
+// Если N=5 -> 0 1 1 2 3
+
+int[] Fibonacci (int num)
+{
+    int[] fArray = new int[num];
+    fArray[0] = 0;
+    fArray[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        fArray[i] = fArray[i - 2] + fArray[i - 1];
+    }
+    return fArray;
+}
+
+Console.Write("Введите число: ");
+int fibNum = Convert.ToInt32(Console.ReadLine());
+if (fibNum >= 2) Console.WriteLine(String.Join(" ", Fibonacci(fibNum)));
+else if (fibNum == 1) Console.WriteLine("1");
+else Console.WriteLine("Нет последовательности");
