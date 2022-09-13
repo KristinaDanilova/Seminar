cc4a31927527356ccb365f2fc6a4812159614821
KristinaDanilova <111421005+KristinaDanilova@users.noreply.github.com>
2022-09-13 11:34:54 +0300

    на вход 3 числа: может ли быть треугольник со сторонами длины

---
+++ Seminar6/Task40/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает 3 числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины
+
+bool Triangle(int a, int b, int c)
+{
+    return (a < b + c && b < a + c && c < b + a);
+}
+
+Console.Write("Введите сторону а: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону b: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону c: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Triangle(num1, num2, num3);
+if (Triangle(num1, num2, num3)) Console.WriteLine("Может сушествовать");
+else Console.WriteLine("Не может существовать");
